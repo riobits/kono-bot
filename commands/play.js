b1ee5b8a65c2e70379ce409c25210fd447cc66a7
riobits <23579928+RioBits@users.noreply.github.com>
2021-01-19 18:47:25 +0300

    fixes

--- commands/play.js
+++ commands/play.js
@@ -12,9 +12,11 @@ module.exports = {
     async execute(message, args) {
         if(message.member.voice.channel) {
             const connection = await message.member.voice.channel.join();
-            const videoId = args[0].slice(args[0].indexOf('watch?v=') + 8);
+            const wURL = args[0].indexOf('watch?v=');
+            const mobileURL = args[0].indexOf('.be/');
+            const videoId = args[0].slice(wURL + 8) || args[0].slice(mobileURL + 4);
 
-            const video = args[0].startsWith('http')? (await yts({videoId})) : (await yts(args.join(' '))).videos[0];
+            const video = await args[0].startsWith('http')? yts({videoId}) : (yts(args.join(' '))).videos[0];
 
             connection.play(ytdl(video.url, { filter: 'audioonly' }))
 
